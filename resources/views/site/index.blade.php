6b5abd114b3fceac73e04a61cdbeceec1d5296a2
ketchanji <valerydjanya8@gmail.com>
2023-01-06 00:43:04 +0100

    updating UI

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,62 +1,43 @@
 <x-layout>
     <x-navbar />
     {{-- ADD CODE FOR HOME PAGE HERE --}}
-    <section class="home" id="home">
-
-        <div class="content">
-            <h3>we've got all <span>great</span> models for your</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam libero nostrum veniam facere tempore nisi.</p>
-            <a href="#" class="btn">Book now</a>
+    <section class="home">
+        <div class="container1">
+            <h1>FAST AND EASY <span>WAY TO RENT</span> A CAR</h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi ullam esse officia quos ex laborum fugit quidem libero dolorum.</P>
         </div>
-
+        <div class="container2">
+            <div class="img"></div>
+        </div>
     </section>
 
-    <div class="container-2">
-      <div class="contain-1">
-        <div class="image">
-
-        </div>
-      </div>
-      <div class="contain-2">
-        <h1>Stay Safe,And Book ride with Us</h1>
-        <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file 
-          storage needs.</p>
-          <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email 
-            attachments required.</p>
-            <button>See how Fylo works <img src="/images/icon-arrow.svg" alt=""> </button>
-      </div>
-    </div>
-
-    <section class="products" id="products">
-
-        <h1 class="heading"> Our <span>vehicles</span> </h1>
-
-        <div class="swiper product-slider">
-
-            <div class="swiper-wrapper">
-
-                <div class="swiper-slide box">
-                    <img src="" alt="">
-                    <h3>Old Luxury Car</h3>
-                    <div class="price"> $4.99/- - 10.99/- </div>
-                    <a href="/contact" class="btn">Book now</a>
-                </div>
-                <div class="swiper-slide box">
-                    <img src="image/product-2.png" alt="">
-                    <h3>Old Luxury Car</h3>
-                    <div class="price"> $4.99/- - 10.99/- </div>
-                    <a href="/contact" class="btn">Book now</a>
-                </div>
-                <div class="swiper-slide box">
-                    <img src="image/product-3.png" alt="">
-                    <h3>Luxury Car</h3>
-                    <div class="price"> $4.99/- - 10.99/- </div>
-                    <a href="/contact" class="btn">Book now</a>
-                </div>
-
+    <section class="form">
+        <form action="">
+            <div class="address">
+                <label for="address">Address</label>
+                <input type="text" id="address" placeholder="ENTER YOUR ADDRESS">
             </div>
+            <div class="Pick-up">
+                <label for="">Pick-up</label>
+                <input type="date" id="Pick-up">
+            </div>
+            <div class="Time">
+                <label for="Time">Time</label>
+                <input type="time" id="Time">
+            </div>
+            <button class="btn">submit</button>
 
+        </form>
+    </section>
+
+    <section class="our-service">
+        <div class="small-container2">
+            <div class="img"></div>
+        </div>
+        <div class="small-container1">
+        <h1>OUR <span>SERVICES</span></h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi ullam esse officia quos ex laborum fugit quidem libero dolorum.</P>
         </div>
     </section>
-    <x-footer />
+
 </x-layout>
