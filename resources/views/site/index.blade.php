93bf185ed39526968ee5b762e0408cacd5cef8cc
ketchanji <valerydjanya8@gmail.com>
2023-01-06 07:30:00 +0100

    updating UI

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -35,8 +35,68 @@
             <div class="img"></div>
         </div>
         <div class="small-container1">
-        <h1>OUR <span>SERVICES</span></h1>
+            <h1>OUR <span>SERVICES</span></h1>
             <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi ullam esse officia quos ex laborum fugit quidem libero dolorum.</P>
+        </div>
+    </section>
+
+    <section class="our-vehicles">
+
+        <h1>OUR <span>VEHICLES</span> </h1>
+
+        <div class="carry">
+
+            <div class="big-container1">
+                <h1>OUR CARs</h1>
+                <ul>
+                    <li>8-speed Automatic</li>
+                    <li>2.5-liter 4-cylinder 203Hp</li>
+                    <li>MODEL: 2532</li>
+                    <li>STOCK: KU680564</li>
+                </ul>
+                <button class="btn2">90$/Day</button>
+            </div>
+
+            <div class="big-container2">
+                <div class="img"></div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="product">
+        <h1>We Have Everything You Need</h1>
+        <div class="main">
+            <div class="card1">
+                <h1>BMW RS8</h1>
+                <img src="" alt="">
+                <ul>
+                    <li>4 sits</li>
+                    <li>2.5-liter 4-cylinder 203Hp</li>
+                    <li>8-speed Automatic</li>
+                </ul>
+                <button class="btn3">Book Now</button>
+            </div>
+            <div class="card1">
+                <h1>BMW W24</h1>
+                <img src="" alt="">
+                <ul>
+                    <li>5 sits</li>
+                    <li>6.5-liter 4-cylinder 203Hp</li>
+                    <li>7-speed Automatic</li>
+                </ul>
+                <button class="btn3">Book Now</button>
+            </div>
+            <div class="card1">
+                <h1>BMW W88</h1>
+                <img src="" alt="">
+                <ul>
+                    <li>5 sits</li>
+                    <li>5.5-liter 4-cylinder 203Hp</li>
+                    <li>9-speed Automatic</li>
+                </ul>
+                <button class="btn3">Book Now</button>
+            </div>
         </div>
     </section>
 
